2d7787b3999129962e6897b24b1cacd4b9c0f8cd
Marja Wahl <marja.wahl@gmx.de>
2023-12-01 11:41:35 -0800

    Update both model notebooks and start loading data for evaluation

--- recommender_model/scripts/ari.py
+++ recommender_model/scripts/ari.py
@@ -8,9 +8,11 @@ from spotipy.oauth2 import SpotifyClientCredentials
 import re
 import os
 
+import spotipy.util as util
+
 def ari_to_features(ari):
 
-    with open("../secret.txt") as f:
+    with open("secret.txt") as f:
         secret_ls = f.readlines()
         cid = secret_ls[0][:-2]
         secret = secret_ls[1]
@@ -26,15 +28,15 @@ def ari_to_features(ari):
     
     #Audio features
     features = sp.audio_features(ari)[0]
-    
+
     #Artist of the track, for genres and popularity
     artist = sp.track(ari)["artists"][0]["id"]
     artist_pop = sp.artist(artist)["popularity"]
     artist_genres = sp.artist(artist)["genres"]
-    
+
     #Track popularity
     track_pop = sp.track(ari)["popularity"]
-    
+
     #Add in extra features
     features["artist_pop"] = artist_pop
     if artist_genres:
@@ -42,7 +44,7 @@ def ari_to_features(ari):
     else:
         features["genres"] = "unknown"
     features["track_pop"] = track_pop
-    
+
     return features
 
 if __name__ == "__main__":
